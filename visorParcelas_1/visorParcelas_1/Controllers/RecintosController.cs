9d7b24ef430252d5ba76aa122377f0fd2c5b8cbc
ssimoanto <simoanto@ucm.es>
2024-03-05 11:15:10 +0100

    Intento de acceder a las columnas

--- visorParcelas_1/visorParcelas_1/Controllers/RecintosController.cs
+++ visorParcelas_1/visorParcelas_1/Controllers/RecintosController.cs
@@ -4,6 +4,7 @@ using Npgsql.Schema;
 using System.Buffers;
 using System.Collections.ObjectModel;
 using System.Data;
+//using Newtonsoft.Json;
 
 namespace visorParcelas_1.Controllers
 {
@@ -38,16 +39,64 @@ namespace visorParcelas_1.Controllers
             //Creación del comando
             NpgsqlCommand command = connection.CreateCommand();
 
-            command.CommandText = $"SELECT * FROM recintos WHERE provincia = {provincia} AND municipio = {municipio} AND agregado = " +
+            command.CommandText = $"SELECT * FROM public.\"t$recinto\" WHERE provincia = {provincia} AND municipio = {municipio} AND agregado = " +
                 $"{agregado} AND zona = {zona} AND poligono = {poligono} AND parcela = {parcela} AND recinto = {recinto}";
 
-            //NpgsqlDataReader reader = command.ExecuteReader();
 
-            //while (reader.Read())
+            NpgsqlDataReader reader = command.ExecuteReader();
+
+            //try
             //{
-            //    Console.WriteLine(reader["nombre"].ToString());
+            if (reader.Read())
+            {
+                // pruebas para ver si encuentro el valos de la columna, solo he encontrado el nombre xd
+                var nombre = reader.GetColumnSchema()[0].ColumnName;
+                Console.WriteLine($"ColumnName: {nombre}");
+                var nombre2 = reader.GetColumnSchema()[0].ColumnOrdinal;
+                Console.WriteLine($"ColumnOrdinal: {nombre2}");
+                var nombre3 = reader.GetColumnSchema()[0].DefaultValue;
+                Console.WriteLine($"DefaultValue: {nombre3}" + nombre3.ToString());
+                var nombre4 = reader.GetColumnSchema()[0].ColumnAttributeNumber;
+                Console.WriteLine($"ColumnAttributeNumber: {nombre4}");
+                var nombre5 = reader.GetColumnSchema()[0].BaseColumnName;
+                Console.WriteLine($"BaseColumnName: {nombre5}" + nombre5.ToString());
+                var nombre6 = reader.GetColumnSchema()[0];
+                Console.WriteLine($".GetColumnSchema()[0]; a secas: {nombre6}" + nombre6);
+
+                //// Creación del objeto GeoJSON
+                //var geoJson = new
+                //{
+                //    type = "Feature",
+                //    geometry = new
+                //    {
+                //        type = "Point",
+                //        coordinates = new[] { longitude, latitude }
+                //    },
+                //    properties = new
+                //    {
+                //        provincia,
+                //        municipio,
+                //        agregado,
+                //        zona,
+                //        poligono,
+                //        parcela,
+                //        recinto,
+                //        altitud,
+                //        pendiente_media = pendienteMedia // Cambia el nombre según tus necesidades
+                //    }
+                //};
+
+                //// Serialización del objeto GeoJSON
+                //var geoJsonString = JsonConvert.SerializeObject(geoJson);
+
+                // Ahora puedes usar 'geoJsonString' como respuesta en tu API REST
+
+            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error al procesar la solicitud");
             //}
         }
-
     }
 }
