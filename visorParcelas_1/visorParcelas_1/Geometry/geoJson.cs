2be15802a146cf517c1f1eafef14033fba60fb13
Ángel López <anlobeinst@gmail.com>
2024-03-06 09:09:56 +0100

    Se muestra el geoJson en pantalla

---
+++ visorParcelas_1/visorParcelas_1/Geometry/geoJson.cs
@@ -0,0 +1,21 @@
+﻿namespace visorParcelas_1.Geometry
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Crs
+    {
+        public string? type { get; set; }
+        public Properties? properties { get; set; }
+    }
+
+    public class Properties
+    {
+        public string? name { get; set; }
+    }
+
+    public class geoJson
+    {
+        public string? type { get; set; }
+        public Crs? crs { get; set; }
+        public List<List<List<double>>>? coordinates { get; set; }
+    }
+}
