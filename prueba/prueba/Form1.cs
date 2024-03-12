2f79acb4816f6de7a1b018f8dfaa45add8f2c6cb
ssimoanto <simoanto@ucm.es>
2024-03-12 09:06:19 +0100

    formato de ayer añadido

--- prueba/prueba/Form1.cs
+++ prueba/prueba/Form1.cs
@@ -1,8 +1,4 @@
-using Newtonsoft.Json;
-using System.Text.Json.Serialization;
-using visorParcelas_1.Controllers;
-using visorParcelas_1.Geometry;
-
+ï»¿
 namespace prueba
 {
     public partial class Form1 : Form
@@ -10,30 +6,70 @@ namespace prueba
         public Form1()
         {
             InitializeComponent();
+            provinciaText.Text = "28";
+            municipioText.Text = "85";
+            agregadoText.Text = "0";
+            zonaText.Text = "0";
+            poligonoText.Text = "1";
+            parcelaText.Text = "1";
         }
 
-        private void vomito_Click(object sender, EventArgs e)
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            lolAsync();
+            resultLabel.Text = "HelloWorld: " + provinciaText.Text + ", " + municipioText.Text + ", " + agregadoText.Text + ", " +
+                                                zonaText.Text + ", " + poligonoText.Text + ", " + parcelaText.Text + ".";
+
+            callAsync();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void provinciaText_TextChanged(object sender, EventArgs e)
         {
-
+            municipioText.Visible = true;
+            municipioLabel.Visible = true;
+        }
+        private void municipioText_TextChanged(object sender, EventArgs e)
+        {
+            agregadoText.Visible = true;
+            agregadoLabel.Visible = true;
         }
 
-        async Task lolAsync()
+        private void agregadoText_TextChanged(object sender, EventArgs e)
         {
-            var url = "https://localhost:7266/Parcela/28/85/0/0/1/1"; // Reemplaza con la URL correcta
+            zonaText.Visible = true;
+            zonaLabel.Visible = true;
+        }
+
+        private void zonaText_TextChanged(object sender, EventArgs e)
+        {
+            poligonoText.Visible = true;
+            poligonoLabel.Visible = true;
+        }
+
+        private void poligonoText_TextChanged(object sender, EventArgs e)
+        {
+            parcelaText.Visible = true;
+            parcelaLabel.Visible = true;
+        }
+
+        private void parcelaText_TextChanged(object sender, EventArgs e)
+        {
+            buscarParcela.Visible = true;
+        }
+
+        // llama a la url
+        async Task callAsync()
+        {
+            var url = "https://localhost:7266/Parcela/" + provinciaText.Text + "/" + municipioText.Text + "/" + agregadoText.Text + "/"
+                                                        + zonaText.Text + "/" + poligonoText.Text + "/" + parcelaText.Text;
             var httpClient = new HttpClient();
             var response = await httpClient.GetAsync(url);
 
             if (response.IsSuccessStatusCode)
             {
                 var geoJsonString = await response.Content.ReadAsStringAsync();
-                resultLabel.Text = geoJsonString.ToString();
-                Root root = JsonConvert.DeserializeObject<Root>(geoJsonString);
-                // Procesa el geojson según tus necesidades
+                resultLabel.Text = geoJsonString.Length.ToString();
+                //Root root = JsonConvert.DeserializeObject<Root>(geoJsonString);
             }
         }
     }
